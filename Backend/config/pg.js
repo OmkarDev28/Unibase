e9a0d4dce1eaeb5b8abac3a70d0631c911266ecd
Omkar Ugalmugale <omkarugalmugale84@gmail.com>
2026-02-26 18:22:52 +0530

    Update pg.js

--- Backend/config/pg.js
+++ Backend/config/pg.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import 'dotenv/config';
 
 const pool = new Pool({
-    connectionString: process.env.PG_URL,
+    connectionString: process.env.PG_URL_IPV6,
     rejectUnauthorized: false
 });
 
